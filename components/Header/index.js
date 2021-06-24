fc417f73529133f9960a729a6ff5fcf4ccfe6566
Abdulhamit Akaslan <hamtaksln@gmail.com>
2021-06-24 12:35:00 +0300

    rename_folders

--- components/Header/index.js
+++
@@ -1,5 +0,0 @@
-function Header() {
-    return <div className="w-full h-16 bg-yellow-400">Header</div>
-}
-
-export default Header
--- components/YTPlayer/index.js
+++
@@ -1,5 +0,0 @@
-function YTPlayer() {
-    return <div className="w-full h-full bg-red-600">YTPlayer</div>
-}
-
-export default YTPlayer
