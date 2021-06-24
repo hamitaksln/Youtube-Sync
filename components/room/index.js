d718960bda0f1aee7e50941f547ced43d78ae686
Abdulhamit Akaslan <hamtaksln@gmail.com>
2021-06-24 12:35:20 +0300

    rename_folders_2

---
+++ components/room/index.js
@@ -0,0 +1,16 @@
+import YTPlayer from "../ytplayer"
+import ChatBox from "../chat-box"
+
+function Room() {
+    return (
+        <div
+            style={{ display: "grid", gridTemplateColumns: "3fr 1fr" }}
+            className="w-full h-full bg-red-400 p-4 gap-4"
+        >
+            <YTPlayer></YTPlayer>
+            <ChatBox></ChatBox>
+        </div>
+    )
+}
+
+export default Room
---
+++ components/ytplayer/index.js
@@ -0,0 +1,5 @@
+function YTPlayer() {
+    return <div className="w-full h-full bg-red-600">YTPlayer</div>
+}
+
+export default YTPlayer
